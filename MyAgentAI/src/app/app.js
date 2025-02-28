edbd0fab98a80fb91252866ae519cf8b23f9b034
Vo Duy Anh <voduyanh1984@gmail.com>
2025-02-28 23:27:48 +0700

    define my agent ai

---
+++ MyAgentAI/src/app/app.js
@@ -0,0 +1,49 @@
+const { MemoryStorage, MessageFactory } = require("botbuilder");
+const path = require("path");
+const config = require("../config");
+
+// See https://aka.ms/teams-ai-library to learn more about the Teams AI library.
+const { Application, ActionPlanner, OpenAIModel, PromptManager } = require("@microsoft/teams-ai");
+
+const { resetMessage } = require("./messages");
+const { createTask, deleteTask } = require('./actions');
+
+// Create AI components
+const model = new OpenAIModel({
+  azureApiKey: config.azureOpenAIKey,
+  azureDefaultDeployment: config.azureOpenAIDeploymentName,
+  azureEndpoint: config.azureOpenAIEndpoint,
+
+  useSystemMessages: true,
+  logRequests: true,
+});
+const prompts = new PromptManager({
+  promptsFolder: path.join(__dirname, "../prompts"),
+});
+const planner = new ActionPlanner({
+  model,
+  prompts,
+  defaultPrompt: "planner",
+});
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application({
+  storage,
+  ai: {
+    planner,
+    enable_feedback_loop: true,
+  },
+});
+
+app.feedbackLoop(async (context, state, feedbackLoopData) => {
+  //add custom feedback process logic here
+  console.log("Your feedback is " + JSON.stringify(context.activity.value));
+});
+
+app.message("reset", resetMessage);
+
+app.ai.action("createTask", createTask);
+app.ai.action("deleteTask", deleteTask);
+
+module.exports = app;
